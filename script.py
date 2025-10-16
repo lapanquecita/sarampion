1d6cbbf6c1f88c5f523717c1dc9fa1622070a896
Montse <79296842+lapanquecita@users.noreply.github.com>
2025-10-16 11:53:22 -0600

    Actualización datos

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ from scipy.stats import bootstrap
 
 
 # La fecha del corte de los datos.
-FECHA_FUENTE = "09/10/2025"
+FECHA_FUENTE = "15/10/2025"
 
 # Estos colores serán la paleta para todas las gráficas.
 PLOT_COLOR = "#1A1A1D"
